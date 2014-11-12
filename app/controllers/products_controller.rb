fc7f42fc4a21e9c0a88a072f539ed11284089ea1
Jimi Vyas <jimipvyas@gmail.com>
2014-11-11 22:46:26 -0500

    changed @contact to #contact to fix error, made it so reviews show up for everyone

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,49 +1,54 @@
 class ProductsController < ApplicationController
   def index
-  	@products = Product.all
+  	$products = Product.all
   end
 
   def show
-  	@product = Product.find(params[:id])
+  	$product = Product.find(params[:id])
+
+  	if current_user
+  		@review = $product.reviews.build
+  	end
+  	
   end
 
   def new
-  	@product = Product.new
+  	$product = Product.new
   end
 
   def edit
-  	@product = Product.find(params[:id])
+  	$product = Product.find(params[:id])
   end
 
   def create
-  	@product = Product.new(product_params)
+  	$product = Product.new(product_params)
 
-  	if @product.save
+  	if $product.save
   		redirect_to products_url
   	else
   		render :new
   	end
   end
-  
+
 
 	def update
-		@product = Product.find(params[:id])
-		if @product.update_attributes(product_params)
-		redirect_to product_path(@product)
+		$product = Product.find(params[:id])
+		if $product.update_attributes(product_params)
+			redirect_to product_path($product)
 		else
-		render :edit
+			render :edit
 		end
 	end
 
 	def destroy
-		@product = Product.find(params[:id])
-		@product.destroy
+		$product = Product.find(params[:id])
+		$product.destroy
 		redirect_to product_path
 	end
 
 	private
 		def product_params
 		params.require(:product).permit(:name, :description, :price_in_cents)
-	end
+		end
 
 end
